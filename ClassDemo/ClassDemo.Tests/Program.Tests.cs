11187e27d87e23ef85a4ec1d22cdcaae5ef5f14b
Zach <zlawson@ewu.edu>
2023-01-12 16:12:46 -0800

    Added Tests

--- ClassDemo/ClassDemo.Tests/Program.Tests.cs
+++ ClassDemo/ClassDemo.Tests/Program.Tests.cs
@@ -1,3 +1,4 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ClassDemo.Tests;
 
 [TestClass]
@@ -8,5 +9,19 @@ public class ProgramTests
     {
         string actual = Program.GetName("Inigo", "Montoya");
         Assert.AreEqual<string>("Inigo Montoya", actual);
+    }
+
+    [TestMethod]
+    public void PlayingWithIntegers()
+    {
+        int number = 42;
+        Assert.AreEqual("System.Int32", number.GetType().FullName);
+    }
+
+    [TestMethod]
+    public void PlayingWithStrings()
+    {
+        string number = "forty-two";
+        Assert.AreEqual("System.String", number.GetType().FullName);
     }
 }

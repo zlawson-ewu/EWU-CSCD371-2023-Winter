8bfb8aaca134ff786a37f7ac1626af28615ebcee
zlawson-ewu <75447307+zlawson-ewu@users.noreply.github.com>
2023-01-13 02:45:21 -0800

    Added double cast to GetPercentCorrect calculation. Added missing assignment of created question object to question array in LoadQuestions. Application no long crashes and all unit tests pass.

--- PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
+++ PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
@@ -24,7 +24,7 @@ namespace PrincessBrideTrivia
 
         public static string GetPercentCorrect(int numberCorrectAnswers, int numberOfQuestions)
         {
-            return (numberCorrectAnswers / numberOfQuestions * 100) + "%";
+            return ((double)numberCorrectAnswers / numberOfQuestions * 100) + "%";
         }
 
         public static bool AskQuestion(Question question)
@@ -89,6 +89,8 @@ namespace PrincessBrideTrivia
                 question.Answers[1] = answer2;
                 question.Answers[2] = answer3;
                 question.CorrectAnswerIndex = correctAnswerIndex;
+
+                questions[i] = question;
             }
             return questions;
         }

fd41409a37dfe13c270fd7febdc6aaa0cb323e69
Mark Michaelis <mark@IntelliTect.com>
2023-01-17 16:33:20 -0800

    1-17-23 second half of lecture

---
+++ Lecture3/ClassDemo/ClassDemo.Tests/Train.Tests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace ClassDemo.Tests;
+
+[TestClass]
+public class TrainTests
+{
+    [TestMethod]
+    public void Create_TrainWith5Carriages_Success()
+    {
+        // Act
+        Train train = new(model: "Steam", carriages: 5);
+
+        // Assert
+        Assert.AreEqual(5, train.Carriages);
+    }
+
+    [TestMethod]
+    public void Create_GivenTrain_IsVehicle()
+    {
+        // Arrange
+        Train train = new("Steam", 6);
+
+        // Assert
+        Assert.IsTrue(train is Vehicle);
+    }
+
+    [TestMethod]
+    public void Create_GivenCarriageAndModel_Success()
+    {
+        // Arrange
+        Train train = new("Bullet", 6);
+
+        // Assert
+        Assert.AreEqual("Bullet", train.Model);
+    }
+
+    [TestMethod]
+    public void Create_GivenNoCarriage_CarriagesEquals42()
+    {
+        // Arrange
+        Train train = new("Bullet");
+
+        // Assert
+        Assert.AreEqual(42, train.Carriages);
+    }
+}
---
+++ Lecture3/ClassDemo/ClassDemo/StringHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Helpers;
+public static class StringHelper
+{
+    // 1.) Class & members must be static
+    // 2.) First parameter must be the datatype you want to extend the method on
+    // 3.) prefixed with keyword 'this'
+    public static string AppendEllipses(this string input)
+    {
+        return $"{input}...";
+    }
+}

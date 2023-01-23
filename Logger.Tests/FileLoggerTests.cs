14d9fd9b9c7cacdf1c790acc826a125ff8a34e81
zlawson-ewu <75447307+zlawson-ewu@users.noreply.github.com>
2023-01-23 15:26:32 -0800

    Added a test and cleaned some stuff

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -1,21 +1,42 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.IO;
+using System.Linq;
 
 namespace Logger.Tests;
 
 [TestClass]
 public class FileLoggerTests
 {
+    string testPath = Path.Combine(Directory.GetCurrentDirectory(), "testLog.txt");
+
     [TestMethod]
-    public void FileLogger_OnCreation_HasCallingClassName()
+    public void FileLogger_OnCreation_SetsClassNameInLogFactory()
     {
-        string testPath = "C:\\Users\\zachl\\Desktop\\test.txt";
+        //Arrange
+        //Act
         LogFactory factory = new();
         factory.ConfigureFileLogger(testPath);
         BaseLogger logger = factory.CreateLogger(GetType().Name);
         string name = logger.ClassName;
-        logger.Log(LogLevel.Information, "This is a test message.");
+        //Assert
         Assert.AreEqual(name, GetType().Name);
     }
-    
+
+    [TestMethod]
+    public void FileLogger_OnLog_AppendsMessageToFile()
+    {
+        //Arrange
+        string date = DateTime.Now.ToString();
+        string testMessage = $" {date} {GetType().Name} {LogLevel.Information} : test";
+        //Act
+        LogFactory factory = new();
+        factory.ConfigureFileLogger(testPath);
+        BaseLogger logger = factory.CreateLogger(GetType().Name);
+        logger.Log(LogLevel.Information, "test");
+        string readLine = File.ReadLines(testPath).Last();
+        //Assert
+        Assert.AreEqual(testMessage, readLine);
+    }
 }

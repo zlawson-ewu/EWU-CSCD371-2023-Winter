18e2cc8f9dfd7f2a2f8539d020804598b508da8c
Mark Michaelis <mark@IntelliTect.com>
2023-02-03 12:56:56 -0800

    Fixed logger classes

---
+++ Logger/FullName.cs
@@ -0,0 +1,7 @@
+﻿namespace Logger;
+public record struct FullName(string FirstName, string LastName, string? MiddleName = null)
+{
+    public string FirstName { get; } = FirstName??throw new ArgumentNullException(nameof(FirstName));
+    public string LastName { get; } = LastName??throw new ArgumentNullException(nameof(LastName));
+    public string? MiddleName { get; } = MiddleName;
+}

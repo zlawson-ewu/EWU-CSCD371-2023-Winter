56ad31bc1da35ae9dce987086ccb3d58f38a5f91
Mark Michaelis <mark@IntelliTect.com>
2023-02-02 16:38:12 -0800

    Code at the end of class

--- Logger.Tests/ProductTests.cs
+++ Logger.Tests/ProductTests.cs
@@ -8,9 +8,9 @@ public class ProductTests
     public void ReadOnly_Property_Set()
     {
         Product product = new Product(
-            Name: "AirJordan", 
-            Price: 1000.00, 
-            Description:"Really expensive shoes - but they are just shoes.");
+            42, Name: "AirJordan",
+            Price: 1000.00,
+            Description: "Really expensive shoes - but they are just shoes.");
         Assert.AreEqual(1000, product.Price);
         // product.Price = 2000.00;
     }
@@ -20,7 +20,7 @@ public class ProductTests
     public void Name_AssignNullInConstructor_ThrowException()
     {
         Product product = new Product(
-            Name: null!,
+            42, Name: null!,
             Price: 1000.00,
             Description: "Really expensive shoes - but they are just shoes.");
         // Assert.AreEqual(1000, product.Price);
@@ -28,19 +28,59 @@ public class ProductTests
     }
 
     [TestMethod]
-    [ExpectedException(typeof(ArgumentNullException))]
     public void Name_AssignNullInInitializer_ThrowException()
     {
         Product product = new Product(
-            Name: "Reebok",
+            42, Name: "Reebok",
             Price: 1000.00,
             Description: "Really expensive shoes - but they are just shoes.")
         { Description = "These are not quite as good as AirJordans" };
         /*{ Name = null! }*/
-        product.Description = "These are NOT quite as good as AirJordans ";
+        // product.Description = "These are NOT quite as good as AirJordans ";
+
         ;
     }
 
+    [TestMethod]
+    public void Equals_TwoReferences_True()
+    {
+        Product product1 = new Product(
+            42, Name: "AirJordan",
+            Price: 1000.00,
+            Description: "Really expensive shoes - but they are just shoes.");
+        Product product2 = product1;
+        Assert.IsTrue(ReferenceEquals(product1, product2));
+    }
+
+    [TestMethod]
+    public void Equals_TwoInstances_True()
+    {
+        Product product1 = new Product(
+            42, Name: "AirJordan",
+            Price: 1000.00,
+            Description: "Really expensive shoes - but they are just shoes.");
+        Product product2 = product1 with { /*Name = "Something",*/ Price = 2000.00 };
+
+        Assert.IsFalse(ReferenceEquals(product1, product2));
+        Assert.IsTrue(product1.Equals(product2));
+        Assert.IsTrue(product1 == product2);
+
+    }
+
+    [TestMethod]
+    public void Equals_TwoInstancesWithDifferentId_True()
+    {
+        Product product1 = new Product(
+            42, Name: "AirJordan",
+            Price: 1000.00,
+            Description: "Really expensive shoes - but they are just shoes.");
+        Product product2 = product1 with {Id = 42, Price = 0 };
+
+        Assert.IsFalse(ReferenceEquals(product1, product2));
+        Assert.IsTrue(product1.Equals(product2));
+        Assert.IsTrue(product1 == product2);
+
+    }
 }
 
 
--- Logger/Product.cs
+++ Logger/Product.cs
@@ -1,15 +1,26 @@
 ﻿namespace Logger;
-public record class Product(string Name, double Price, string? Description)
+public sealed record class Product(int Id, string Name, double Price, string? Description)
 {
     public string Name { get; } = Name ?? throw new ArgumentNullException(nameof(Name));
-    public double Price { get; set; } = Price;
-}
+    public double Price { get; init; } = Price;
 
-public record class SuperProduct(string Name, double Price, string? Description, double Discount) : 
-    Product(Name, Price, Description)
+    public bool Equals(Product? other)
+    {
+        if (other is null) return false;
+        return (Id, Name) == (other.Id, other.Name);
+    }
+
+    public override int GetHashCode()
+    {
+        return (Id, Name).GetHashCode();
+    }
+
+#if NOTSEALED
+public record class SuperProduct(int Id, string Name, double Price, string? Description, double Discount) : 
+    Product(42, Name, Price, Description)
 {
 }
+#endif // NOTSEALED
 
 
-
-
+}

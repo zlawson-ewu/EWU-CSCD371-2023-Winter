31bcdda9c7a5bff1290b830f69e8d3a0b6aec3c0
Zach <zlawson@ewu.edu>
2023-02-04 22:51:46 -0800

    From class

--- Logger.Tests/ProductTests.cs
+++ Logger.Tests/ProductTests.cs
@@ -7,9 +7,64 @@ public class ProductTests
     [TestMethod]
     public void ReadOnly_Property_Set()
     {
+        Product product = new Product(
+            Name: "AirJordan", 
+            Price: 1000, 
+            Description: "Really Expensive");
+        Assert.AreEqual(1000, product.Price);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void Name_AssignNullInConstructor_ThrowException()
+    {
+        Product product = new Product(
+            Name: null!,
+            Price: 1000,
+            Description: "Really Expensive");
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void Name_AssignNullInInitializer_ThrowException()
+    {
+        Product product = new Product(
+            Name: "Reebok",
+            Price: 1000,
+            Description: "Really Expensive")
+        { Description = "These are not as good as AirJordans" };
+        //{ Name = null! };
 
     }
-    
+
+    [TestMethod]
+    public void Equals_TwoReferences_True()
+    {
+        Product product1 = new Product(
+            Name: "Reebok",
+            Price: 1000,
+            Description: "Really Expensive");
+
+        Product product2 = product1;
+
+        Assert.IsTrue(ReferenceEquals(product1, product2));
+    }
+
+    [TestMethod]
+    public void Equals_TwoInstancesWithDifferentId_True()
+    {
+        Product product1 = new Product(
+            Id: 42,
+            Name: "Reebok",
+            Price: 1000,
+            Description: "Really Expensive");
+
+        Product product2 = product1 with { Id = 490, Price = 2000 };
+
+        Assert.IsFalse(ReferenceEquals(product1, product2));
+        Assert.IsTrue(product1.Equals(product2));
+        Assert.IsTrue(product1== product2);
+    }
 }
 
 
--- Logger/Product.cs
+++ Logger/Product.cs
@@ -1,8 +1,17 @@
 ﻿namespace Logger;
-public class Product
+public sealed record class Product(int Id, string Name, double Price, string? Description)
 {
+    public string Name { get; } = Name ?? throw new ArgumentNullException(nameof(Name));
+    public double Price { get; set; } = Price;
 
-    
+    public bool Equals(Product? other)
+    {
+        if (other is null) return false;
+        return (Id, Name) == (other.Id, other.Name);
+    }
+
+    public override int GetHashCode()
+    {
+        return (Id, Name).GetHashCode();
+    }
 }
-
-

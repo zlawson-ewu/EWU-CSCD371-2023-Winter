01fab4c5b22b56c80c73e67e368ba68746a3664f
zlawson-ewu <75447307+zlawson-ewu@users.noreply.github.com>
2023-02-11 20:48:30 -0800

    Changed name of class and added inline node class with assumption that "Create a node class" does not mean to call the linked list "node".

--- GenericsHomework/GenericsHomework.Test/NodeTests.cs
+++ GenericsHomework/GenericsHomework.Test/NodeTests.cs
@@ -6,7 +6,7 @@ public class NodeTests
     [TestMethod]
     public void Node_ToString_works()
     {
-        Node<string> node = new("I hope this works...");
+        CircularList<string> node = new("I hope this works...");
         Assert.AreEqual(node.ToString(), "I hope this works...");
     }
 }

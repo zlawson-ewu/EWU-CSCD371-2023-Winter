43eed9aa32862fbe7b1eb13127b882195bd9ed05
zlawson-ewu <75447307+zlawson-ewu@users.noreply.github.com>
2023-02-11 21:56:13 -0800

    Doesn't seem right, reverted naming

---
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -0,0 +1,28 @@
+﻿namespace GenericsHomework;
+
+public class Node<TValue>
+{
+    public TValue? Value { get; set; }
+    public Node<TValue> Next { get; private set; }
+    public Node(TValue? value)
+    {
+        Value = value;
+        Next = this;
+    }
+
+    public void Append(TValue value)
+    {
+        Node<TValue> temp = Next;
+        Next = new(value) { Next = temp };
+    }
+
+    public void Clear()
+    {
+
+    }
+
+    public override string? ToString()
+    {
+        return Value!.ToString();
+    }
+}

3425cae05168cd3fe0a06566c7196b51c1aae3b6
Mark Michaelis <mark@IntelliTect.com>
2023-02-14 15:36:42 -0800

    Code before break

--- Lectures/LambdaExpressions/Lambdas.cs
+++ Lectures/LambdaExpressions/Lambdas.cs
@@ -1,6 +1,94 @@
-﻿namespace LambdaExpressions;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LambdaExpressions;
 
 [TestClass]
 public class Lambdas
 {
+    public bool AlphabeticalGreaterThan(string a, int b)
+    {
+        return a.CompareTo(b) > 0;
+    }
+
+    public bool AlphabeticalGreaterThan(string a, string b)
+    {
+        return a.CompareTo(b) > 0;
+    }
+
+    public string[] Sort(Func<string,string, bool> greaterThan, string[] items )
+    {
+        return null;
+    }
+
+
+
+    [TestMethod]
+    public void Sort_AB_Sucess()
+    {
+        string[] items = new[] { "A", "B" };
+        string[] sortedItems = Sort(AlphabeticalGreaterThan, items);
+        foreach (string item in sortedItems)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public void Foreach(Action<string> action, string[] items)
+    {
+        foreach (string item in items)
+        {
+            action(item);
+        }
+    }
+
+    [TestMethod]
+    public void ForEach_AB_Sucess()
+    {
+        string[] items = new[] { "A", "B" };
+        Foreach(DoSomething, items);
+    }
+
+    void DoSomething(string text) => Console.WriteLine(text);
+
+
+    [TestMethod]
+    public void ForEach_LambdaExpresion_Sucess()
+    {
+        string[] items = new[] { "A", "B" };
+        string sentence = "This is a really long string.";
+        Action<string> action = DoSomething;
+        action = (string text) => Console.WriteLine(text);
+        action = (text) => Console.WriteLine(text);
+        action = text => Console.WriteLine(text);
+        action = Console.WriteLine;
+        action = text =>
+        {
+            Console.WriteLine(text);
+            Console.WriteLine(sentence);
+        };
+        Action takeAction = () => Console.WriteLine(sentence);
+        takeAction = Console.WriteLine;
+
+        Foreach(DoSomething, items);
+    }
+
+
+
+
+
+
+    [TestMethod]
+    [ExpectedException(typeof(Exception))]
+    public void ExceptionHandling()
+    {
+        try
+        {
+
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+        
+    }
 }

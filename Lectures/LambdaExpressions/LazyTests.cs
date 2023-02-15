6d8f8aa9aa1123aa1e263fdea60762ff43c65c24
Mark Michaelis <mark@IntelliTect.com>
2023-02-14 16:20:52 -0800

    Lazy demo

---
+++ Lectures/LambdaExpressions/LazyTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LambdaExpressions;
+
+[TestClass]
+public class LazyTests
+{
+    [TestMethod]
+    public void InvokeLazy()
+    {
+        string text = "Very hard thing to create";
+        Lazy<string> textFactory = new(
+            () => text
+            );
+        Assert.AreEqual(text, textFactory.Value);
+    }
+    
+}

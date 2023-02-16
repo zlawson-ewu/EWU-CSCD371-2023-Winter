073d1098ff58a4df3a93b6b4f1623ca95310c689
Mark Michaelis <mark@IntelliTect.com>
2023-02-16 15:19:11 -0800

    Code at break time

---
+++ Lectures/LambdaExpressions/FullName.cs
@@ -0,0 +1,27 @@
+﻿namespace LambdaExpressions;
+public record FullName(string FirstName, string LastName)
+{
+    public FullName(string fullName): this(null!, null!)
+    {
+        if (fullName.Split(" ") is [string firstName, string lastName ])
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+
+    // Use pattern matching to return the initials of the FullName
+    // Implement a Name property or override ToString() to return the full name.
+    // Hints:
+    //      Using multiple list patterns
+    //      User recursion pattern matching
+
+
+    public string Initials 
+    { 
+        get
+        {
+            return null;
+        }
+    }
+}
---
+++ Lectures/LambdaExpressions/PatternMatchingTests.cs
@@ -0,0 +1,23 @@
+﻿namespace LambdaExpressions;
+
+[TestClass]
+public class PatternMatchingTests
+{
+    
+    [TestMethod]
+    public void SetName_InigoMontoya_Success()
+    {
+        FullName testName = new FullName("Inigo Montoya");
+        (string, string) actual = (testName.FirstName, testName.LastName);
+        Assert.AreEqual<(string, string)>(("Inigo","Montoya"), actual);
+        Assert.IsTrue(testName.FirstName is "Inigo");
+    }
+
+    [TestMethod]
+    public void UsingSwitch_InitialsInigoMontoya_Success()
+    {
+        FullName testName = new FullName("Inigo Montoya");
+        string actuals = testName.Initials;
+        Assert.AreEqual<string>("IM", actuals);
+    }
+}

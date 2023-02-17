6966f57a63f8f3fa946c77775d7b42f5b4b95901
Mark Michaelis <mark@IntelliTect.com>
2023-02-16 16:53:38 -0800

    Code at the end of class

--- Lectures/LambdaExpressions/FullName.cs
+++ Lectures/LambdaExpressions/FullName.cs
@@ -1,9 +1,12 @@
-﻿namespace LambdaExpressions;
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace LambdaExpressions;
 public record FullName(string FirstName, string LastName)
 {
     public FullName(string fullName): this(null!, null!)
     {
-        if (fullName.Split(" ") is [string firstName, string lastName ])
+
+        if (fullName.Split(" ") is [string { Length: >0 } firstName, string { Length: >0 } lastName])
         {
             FirstName = firstName;
             LastName = lastName;
@@ -21,7 +24,19 @@ public record FullName(string FirstName, string LastName)
     { 
         get
         {
-            return null;
+            
+            string result;
+            result = $"{FirstName[0]}{LastName[0]}";
+            if ((FirstName.First(), LastName.First()) is (char firstInitial, char secondInitial))
+                result = new string(new char[] { firstInitial, secondInitial });
+            int[] numbers = new int[] { };
+            List<double> doubleNumbers = null;
+            if (Name.Split(" ") is [[char firstIntial,.. ], [char lastIntial, ..]])
+            {
+                result = $"{firstInitial}{lastIntial}";
+            }
+            return result;
         }
     }
+    public string Name => $"{FirstName} {LastName}";
 }
--- Lectures/LambdaExpressions/PatternMatchingTests.cs
+++ Lectures/LambdaExpressions/PatternMatchingTests.cs
@@ -1,4 +1,6 @@
-﻿namespace LambdaExpressions;
+﻿using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LambdaExpressions;
 
 [TestClass]
 public class PatternMatchingTests
@@ -8,7 +10,8 @@ public class PatternMatchingTests
     public void SetName_InigoMontoya_Success()
     {
         FullName testName = new FullName("Inigo Montoya");
-        (string, string) actual = (testName.FirstName, testName.LastName);
+        (string FirstName, string LastName) actual1 = (testName.FirstName, testName.LastName);
+        var actual = (testName.FirstName, testName.LastName);
         Assert.AreEqual<(string, string)>(("Inigo","Montoya"), actual);
         Assert.IsTrue(testName.FirstName is "Inigo");
     }
@@ -20,4 +23,40 @@ public class PatternMatchingTests
         string actuals = testName.Initials;
         Assert.AreEqual<string>("IM", actuals);
     }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Create_EmptyStrings_Fails()
+    {
+        _ = new FullName("", "");
+    }
+
+    [TestMethod]
+    public void MyTestMethod()
+    {
+        FullName name = new FullName("Inigo Montoya");
+        _ = name switch
+        {
+            (string { Length: >0 and < 10 }, string { Length: >0 and < 10 }) => true,
+        };
+    }
+
+    [TestMethod]
+    public void LINQ_Where()
+    {
+        string name = "Inigo Montoya";
+        IEnumerable<char> oCharacters = name.Where(c => 
+            c == 'o');
+
+        int count = 0;
+        List<int> indiciesWitho = name.Where(c =>
+        {
+            count++;
+            return c=='o';
+        }).Select(c =>
+            count++).ToList();
+        Assert.IsTrue(indiciesWitho is [4, 7, 10]);
+    }
+
+    
 }

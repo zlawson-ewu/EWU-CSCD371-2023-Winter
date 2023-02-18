a2a141d96eabc7bde17647e55aac5cffd6a404fd
zlawson-ewu <75447307+zlawson-ewu@users.noreply.github.com>
2023-02-18 14:35:56 -0800

    Added Dictionary and math methods to Calculator.

--- Calculate/Calculate/Calculator.cs
+++ Calculate/Calculate/Calculator.cs
@@ -6,4 +6,33 @@ public class Calculator
 {
     public delegate void Calculation(string lineIn);
 
+    public IReadOnlyDictionary<char, Func<double, double, double>> MathematicalOperations { get; }
+        = new Dictionary<char, Func<double, double, double>>()
+        {
+            ['+'] = Add,
+            ['-'] = Subtract,
+            ['*'] = Multiply,
+            ['/'] = Divide
+        };
+
+    public static double Add(double x, double y)
+    {
+        return x + y;
+    }
+
+    public static double Subtract(double x, double y)
+    {
+        return x - y;
+    }
+
+    public static double Multiply(double x, double y) 
+    { 
+        return x * y;
+    }
+
+    public static double Divide(double x, double y)
+    {
+        return x / y;
+    }
+
 }

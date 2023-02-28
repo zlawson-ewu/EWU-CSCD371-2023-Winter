37c362859776ccf6dfb1ccda9e6b25c037155654
Mark Michaelis <mark@IntelliTect.com>
2023-02-28 13:58:56 -0800

    Code at the end of class

---
+++ Lectures/LINQ/CheckingDelegatesForNull.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Week_07_LINQ;
+[TestClass]
+public class CheckingDelegatesForNull
+{
+    [TestMethod]
+    public void Null_Delegate_Checking()
+    {
+        bool isInvoked = false;
+        Action action = new Action(() =>isInvoked = true);
+        action();
+        Assert.IsTrue(isInvoked);
+    }
+
+    [TestMethod]
+    public void Action_Delegate_CheckingInlineFunction()
+    {
+        bool isInvoked = false;
+        void SetInvoked()
+        {
+            isInvoked = true;
+        }
+
+        Action action = new Action(SetInvoked);
+        //action();
+        SetInvoked();
+        Assert.IsTrue(isInvoked);
+    }
+
+    public Action? Action { get; set; } = null;
+    
+    [TestMethod]
+    public void Null_Delegate_CheckingInlineFunction()
+    {
+        bool isInvoked = false;
+        Action = () => { isInvoked = true; };
+        
+        Action?.Invoke();
+
+        // /Equivalent
+        //Action? temp = Action;
+        //if (temp != null)
+        //{
+        //    temp.Invoke();
+        //}
+
+        Assert.IsTrue(isInvoked);
+    }
+}
---
+++ Lectures/LINQ/DeferredExecution.cs
@@ -0,0 +1,43 @@
+﻿namespace Week_07_LINQ;
+[TestClass]
+public class DeferredExecution
+{
+    [TestMethod]
+    public void MyTestMethod()
+    {
+        int count = 0;
+        List<int> numbers = Enumerable.Range(1, 10).ToList();
+
+        Assert.AreEqual<int>(10, numbers.Count);
+        IEnumerable<int> evens = numbers.Where(item =>
+        {
+            count++;
+            return item % 2 == 0;
+        });
+        List<int> evens2 = evens.ToList();
+        Assert.AreEqual<int>(10, count);
+        Assert.AreEqual<int>(5, evens2.Count());
+        Assert.AreEqual<int>(10, count);
+        IEnumerable<int> triples = evens2.Where(item =>
+        {
+            count++;
+            return item % 3 == 0;
+        }); // returns the query, not the results
+        Assert.AreEqual<int>(1, triples.Count());
+
+        Assert.AreEqual<int>(15, count);
+        foreach (int item in evens)
+        {
+            Console.WriteLine(item);
+        }
+        Assert.AreEqual<int>(25, count);
+        foreach (int item in evens)
+        {
+            Console.WriteLine(item);
+        }
+        Assert.AreEqual<int>(35, count);
+        
+        // Look at Enumerable class and linq methods *** 
+        
+    }
+}

2c316e778d4eb3f77f21694dfac4e9ec5aa4031a
Mark Michaelis <mark@IntelliTect.com>
2023-02-28 15:07:04 -0800

    Added Week08 project

--- Lectures/CustomCollections/DictionaryTests.cs
+++ Lectures/CustomCollections/DictionaryTests.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Week_07_LINQ;
+
 [TestClass]
 public class DictionaryTests
 {

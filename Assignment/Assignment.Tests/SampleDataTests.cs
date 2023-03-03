10ac3f0c482196b1cf5ecd620f1f94230f96e486
zlawson-ewu <75447307+zlawson-ewu@users.noreply.github.com>
2023-03-02 21:11:23 -0800

    Sync'd with Tom, carried over change: use of Count property instead of method in unique sorted list of states test.

--- Assignment/Assignment.Tests/SampleDataTests.cs
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -15,12 +15,12 @@ public class SampleDataTests
     {
         new Address("507 N Howard St", "Spokane", "WA", "99201"),     //River Front Park
         new Address("803 W Mallon Ave","Spokane", "WA", "99201"),     //David's Pizza
-        new Address("1702 S Grand Blvd", "Spokane", "WA", "99203"),   //Manito Park
-        new Address("101 W 8th Ave", "Spokane", "WA", "99204"),       //Sacred Heart Hospital
-        new Address("601 E Riverside Ave", "Spokane", "WA", "99202"), //Catalyst Building
-        new Address("916 W 2nd Ave", "Spokane", "WA", "99201"),       //Wild Sage Bistro
-        new Address("501 W Park Pl", "Spokane", "WA", "99205"),       //Corbin Park
-        new Address("1810 N Greene St", "Spokane", "WA", "99217")     //Spokane Community College
+        new Address("1702 S Grand Blvd", "Spokane", "AZ", "99203"),   //Manito Park
+        new Address("101 W 8th Ave", "Spokane", "IL", "99204"),       //Sacred Heart Hospital
+        new Address("601 E Riverside Ave", "Spokane", "TN", "99202"), //Catalyst Building
+        new Address("916 W 2nd Ave", "Spokane", "ID", "99201"),       //Wild Sage Bistro
+        new Address("501 W Park Pl", "Spokane", "CA", "99205"),       //Corbin Park
+        new Address("1810 N Greene St", "Spokane", "NY", "99217")     //Spokane Community College
     };
 
     [TestMethod]
@@ -74,7 +74,7 @@ public class SampleDataTests
     }
 
     [TestMethod]
-    public void SpokaneAddresses_YieldOneDistinctState_True()
+    public void HardCodedAddresses_YieldOneDistinctState_True()
     {
         // Arrange
         List<string> uniqueStates = new();
@@ -88,8 +88,8 @@ public class SampleDataTests
             }
         }
 
-        // Assert
-        Assert.AreEqual<int>(1, uniqueStates.Count);
+        //Assert
+        Assert.AreEqual<int>(7, uniqueStates.Count);
     }
 
     [TestMethod]

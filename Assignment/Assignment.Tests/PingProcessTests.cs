a37eda62c90d36ae1953948ac4b5713d7ff894ea
zlawson-ewu <75447307+zlawson-ewu@users.noreply.github.com>
2023-03-15 16:08:40 -0700

    Added Arrange/Act/Assert to tests

--- Assignment/Assignment.Tests/PingProcessTests.cs
+++ Assignment/Assignment.Tests/PingProcessTests.cs
@@ -22,15 +22,23 @@ public class PingProcessTests
     [TestMethod]
     public void Start_PingProcess_Success()
     {
+        // Arrange
+        // Act
         Process process = Process.Start("ping", "localhost");
         process.WaitForExit();
+
+        // Assert
         Assert.AreEqual<int>(0, process.ExitCode);
     }
 
     [TestMethod]
     public void Run_GoogleDotCom_Success()
     {
+        // Arrange
+        // Act
         int exitCode = Sut.Run("google.com").ExitCode;
+
+        // Assert
         Assert.AreEqual<int>(0, exitCode);
     }
 
@@ -38,7 +46,11 @@ public class PingProcessTests
     [TestMethod]
     public void Run_InvalidAddressOutput_Success()
     {
+        // Arrange
+        // Act
         (int exitCode, string? stdOutput) = Sut.Run("badaddress.king[");
+
+        // Assert
         Assert.IsFalse(string.IsNullOrWhiteSpace(stdOutput));
         stdOutput = WildcardPattern.NormalizeLineEndings(stdOutput!.Trim());
         Assert.AreEqual<string?>(
@@ -51,27 +63,37 @@ public class PingProcessTests
     [TestMethod]
     public void Run_CaptureStdOutput_Success()
     {
+        // Arrange
+        // Act
         PingResult result = Sut.Run("localhost");
+
+        // Assert
         AssertValidPingOutput(result);
     }
 
     [TestMethod]
     public void RunTaskAsync_Success()
     {
+        // Arrange
+        // Act
         Task<PingResult> task = Sut.RunTaskAsync("localhost");
         PingResult result = task.Result;
-
         task.Wait();
+
+        // Assert
         AssertValidPingOutput(result);
     }
 
     [TestMethod]
     public void RunAsync_UsingTaskReturn_Success()
     {
+        // Arrange
+        // Act
         Task<PingResult> task = Sut.RunAsync("localhost");
         PingResult result = task.Result;
-
         task.Wait();
+        
+        // Assert
         AssertValidPingOutput(result);
     }
 
@@ -79,9 +101,11 @@ public class PingProcessTests
     async public Task RunAsync_UsingTpl_Success()
     {
         // DO use async/await in this test.
+        // Arrange
+        // Act
         PingResult result = await Sut.RunAsync("localhost");
 
-        // Test Sut.RunAsync("localhost");
+        // Assert
         AssertValidPingOutput(result);
     }
 
@@ -90,18 +114,25 @@ public class PingProcessTests
     [ExpectedException(typeof(AggregateException))]
     public void RunAsync_UsingTplWithCancellation_CatchAggregateExceptionWrapping()
     {
+        // Arrange
         CancellationTokenSource tokenSource = new();
+
+        // Act
         Task<PingResult> result = Task.Run(() => Sut.RunAsync("localhost", tokenSource.Token));
         tokenSource.Cancel();
         result.Wait();
+
+        // Assert
     }
 
     [TestMethod]
     [ExpectedException(typeof(TaskCanceledException))]
     public void RunAsync_UsingTplWithCancellation_CatchAggregateExceptionWrappingTaskCanceledException()
     {
-        // Use exception.Flatten()
+        // Arrange
         CancellationTokenSource tokenSource = new();
+
+        // Act
         try
         {
             Task<PingResult> result = Task.Run(() => Sut.RunAsync("localhost", tokenSource.Token));
@@ -116,15 +147,20 @@ public class PingProcessTests
                 throw inner;
             }
         }
+        // Assert
     }
 
     [TestMethod]
     async public Task RunAsync_MultipleHostAddresses_True()
     {
-        // Pseudo Code - don't trust it!!!
+        // Arrange
         string[] hostNames = new string[] { "localhost", "localhost", "localhost", "localhost" };
         int expectedLineCount = PingOutputLikeExpression.Split(Environment.NewLine).Length*hostNames.Length;
+
+        // Act
         PingResult result = await Sut.RunAsync(hostNames);
+
+        // Assert
         int? lineCount = result.StdOutput?.Split(Environment.NewLine).Length;
         Assert.AreEqual(expectedLineCount, lineCount);
     }
@@ -132,18 +168,26 @@ public class PingProcessTests
     [TestMethod]
     async public Task RunLongRunningAsync_UsingTpl_Success()
     {
-        PingResult result = default;
-        result = await Sut.RunLongRunningAsync("localhost");
+        // Arrange
+        // Act
+        PingResult result = await Sut.RunLongRunningAsync("localhost");
+
+        // Assert
         AssertValidPingOutput(result);
     }
 
     [TestMethod]
     public void StringBuilderAppendLine_InParallel_IsNotThreadSafe()
     {
+        // Arrange
         IEnumerable<int> numbers = Enumerable.Range(0, short.MaxValue);
         System.Text.StringBuilder stringBuilder = new();
+
+        // Act
         numbers.AsParallel().ForAll(item => stringBuilder.AppendLine(""));
         int lineCount = stringBuilder.ToString().Split(Environment.NewLine).Length;
+
+        // Assert
         Assert.AreNotEqual(lineCount, numbers.Count()+1);
     }
 
